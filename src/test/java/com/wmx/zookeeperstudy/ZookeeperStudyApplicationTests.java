fd8de7b9e1cbe1b372995bd6e6b4b8a566d48f2f
wangmaoxiong <2268461750@qq.com>
2020-08-03 19:24:57 +0800

    项目初始化！

---
+++ src/test/java/com/wmx/zookeeperstudy/ZookeeperStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wmx.zookeeperstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
